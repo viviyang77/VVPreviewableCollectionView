30ea99d9c6de4fc3b62d1125c0ea7f077281fb05
Vivi <viviyang@cathayholdings.com.tw>
2020-01-12 13:08:14 +0800

    Initial Commit

---
+++ VVPreviewableCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VVPreviewableCollectionView
+//
+//  Created by Vivi on 2020/1/12.
+//  Copyright © 2020 Vivi Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
